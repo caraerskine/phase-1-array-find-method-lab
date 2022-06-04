c9c1c166d155ec4a24c9f32a3f7e04f31ede8ecb
CaraErskine <caraerskine@gmail.com>
2022-06-04 15:34:38 -0400

    commit tree fa05bdf150f0167e6be46db8956acb78bf9e8953

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]// code your solution here
+
+ function baseball(elem){
+    return elem.result === "W"  
+} 
+
+
+
+function superbowlWin(record){
+const win = record.find(baseball)
+    console.log(record)
+    return win ? win.year : undefined
+
+// return win && win.year
+// if (win){
+    // return win.year
+    //}
+}
+
+
